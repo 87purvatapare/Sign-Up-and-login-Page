9d2ffcc27801aa81eba2866a12b13228567f1ae6
87purvatapare <160571378+87purvatapare@users.noreply.github.com>
2025-01-27 11:27:37 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+
+
+function show(preventDefault){
+    let abc=document.getElementById("u1").value;
+    let abcc =document.getElementById("u2").value;
+    let abb=document.getElementById("m1").value;
+    console.log(abc);
+    console.log(abcc);
+    console.log(abb); 
+}
+
